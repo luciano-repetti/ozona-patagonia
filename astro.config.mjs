652d782caa7956515942f69bc101c56cda36e7d6
luciano-repetti <lucianorepetti2020@gmail.com>
2025-04-26 21:42:03 -0300

    add image logo marca registry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+      hmr: true
+    }
+  }
+});
